15cd450ed14853eb0bd3d7f80b3e71f49c654b50
Cassiano Montanari <cassianomontanari@gmail.com>
2019-04-16 22:48:28 -0300

    Comments and treating 404 error

--- src/app/controllers/CompareController.js
+++ src/app/controllers/CompareController.js
@@ -2,6 +2,7 @@
 
 class CompareController {
   constructor () {
+    // Binding functions so I can use `this`
     this.compareProducts = this.compareProducts.bind(this)
     this.calculate = this.calculate.bind(this)
     this.isValid = this.isValid.bind(this)
@@ -9,6 +10,10 @@ class CompareController {
   }
 
   isValid (consumption) {
+    // If consumption value is true to `isNaN()` comparision (it's not a number)
+    // or if it's of type `boolean``
+    // or if it's above 0 it will return false, so it's not valid
+
     return !(
       Number.isNaN(Number(consumption)) ||
       typeof consumption === 'boolean' ||
@@ -17,9 +22,11 @@ class CompareController {
   }
 
   async compareProducts (consumption) {
+    // Gets the product list from `products.js`
     const productsList = require('../products')
 
     if (!this.isValid(consumption)) {
+      // If it's not a valid number for calculate, it will return a error to display in the front-end
       return {
         products: productsList,
         consumption,
@@ -29,15 +36,19 @@ class CompareController {
 
     let products = productsList
       .map((product) => {
+        // Iterate the product with it's own properties, plus the annual cost
         return {
           ...product,
           annualCost: this.calculate(consumption, product)
         }
       })
       .sort((a, b) => {
+        // Compare which product is cheaper
         a.isCheaper = parseFloat(a.annualCost) < parseFloat(b.annualCost)
         b.isCheaper = parseFloat(b.annualCost) < parseFloat(a.annualCost)
       })
+
+    // If everything is calculated, it returns the product list and consumption value to the `render` method
 
     return {
       products,
@@ -46,10 +57,12 @@ class CompareController {
   }
 
   calculate (consumption, product) {
+    // The `range` constnt will receive the price range which the consumption is between
     const range = product.prices.filter(({ range: [min, max] }) => {
       return min <= consumption && consumption <= max
     })
 
+    // The `prices` constant will receive the calculation that is already a property of the product based on the range it's between
     const prices = range.map(({ calc }) => {
       return calc(consumption)
     })
@@ -65,6 +78,7 @@ class CompareController {
 
     this.compareProducts(consumption).then(
       ({ products, consumption, error = null }) => {
+        // Result will render the view with the products with annual cost if it's a valid consumption, the consumption value and the error
         res.render('compare/index', { products, consumption, error })
       }
     )
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const CompareController = require('./app/controllers/CompareController')
 const DashboardController = require('./app/controllers/DashboardController')
 
 routes.get('/', DashboardController.index)
+routes.get('/compare', DashboardController.index)
 
 routes.post('/compare', CompareController.render)
 
